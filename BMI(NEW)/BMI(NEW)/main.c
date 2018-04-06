4d6e77eaa39995c60d22d92f0ab7d0b57cceb71c
Inyouchange <38124145+Inyouchange@users.noreply.github.com>
2018-04-07 00:37:31 +0800

    Add files via upload

---
+++ BMI(NEW)/BMI(NEW)/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(void) {
+	double weight;
+	double height;
+	printf("Please enter your weight In Kilograms and height In Meters.\n");
+	printf("And I will tell you the BMI: ");
+	scanf_s("%lf%lf", &weight, &height);
+
+	double BMI;
+	BMI = weight / (height * height);
+
+	if (BMI < 18.5) {
+		printf("Underweight\n");
+	}
+	if (18.5 <= BMI && BMI < 24.9) {
+		printf("Normal\n");
+	}
+	if (24.9 <= BMI && BMI < 29.9) {
+		printf("Overweight\n");
+	}
+	if (BMI >= 30) {
+		printf("Obese\n");
+	}
+}
